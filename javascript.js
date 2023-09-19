4b98f72d374b25e1c9747281a0787037b3085311
kkaps002 <kapsimaliscostas@gmail.com>
2023-09-19 09:03:30 +0100

    Pseudocode

---
+++ javascript.js
@@ -0,0 +1,14 @@
+//PSEUDOCODE 
+/*
+    1.initialize round count to 0 and score count set to 0-0
+    2. ask player what they choose
+    3.when the player decides, generate random choice for computer
+    4.compare the two choices
+    5.return result of comparison
+    6. update score 
+    7. repeat process 5 times
+    8. stop game at round 5 (exit repetition loop?)
+    8.show game result
+*/
+
+
