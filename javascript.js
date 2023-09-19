93ce4a8e9bce09e628a8fd3fe8fb00e8cf28a5cc
kkaps002 <kapsimaliscostas@gmail.com>
2023-09-19 11:32:51 +0100

    added HTML title and some text, created getComputerChoice,getPlayerChoice and playRound functions, in playerChoice included checks for unexpected or null inputs, playRound logs player and computer inputs and the result of comparison

--- javascript.js
+++ javascript.js
@@ -12,3 +12,77 @@
 */
 
 
+function getPlayerChoice() {
+    let playerInput = prompt("Rock, Paper or Scissors?")
+    let playerChoice = "_"; // string has content to not trigger null condition
+
+    if (playerInput === null) { // first check if player presses cancel on the prompt, cannot check later because using .toUpperCase on null causes TypeError
+        alert("You need to make a choice!");
+        return getPlayerChoice(); // exit the function and call it again 
+    }
+    else if (playerInput.toUpperCase() === "Rock".toUpperCase()) { //convert both to upper case to make case insensitive
+        playerChoice = "Rock";
+    }
+    else if (playerInput.toUpperCase() === "Paper".toUpperCase()) { 
+        playerChoice = "Paper";
+    }
+    else if (playerInput.toUpperCase() === "Scissors".toUpperCase()) { 
+        playerChoice = "Scissors";
+    }
+    else {
+        //console.log("Unknown");
+        //playerChoice = "Unknown Player Choice" // if choice unknown ask again until choice is valid
+        alert("Make a valid choice!");  
+        return getPlayerChoice(); // exit the function and call it again 
+    }
+
+    return playerChoice; // return valid inputs
+}
+
+function getComputerChoice() {
+    
+    let randomNum = Math.floor(Math.random() * ((3-1)+1) + 1); //generates 1,2 or 3
+    let computerChoice = "";
+
+    // switch statement because the number of choices is finite 
+   switch(randomNum) {
+    case 1:
+        computerChoice = "Rock";
+      break;
+    case 2:
+        computerChoice = "Paper";
+      break;
+    case 3:
+        computerChoice = "Scissors";
+  }
+
+  return computerChoice; // value to be sent to other functions
+}
+
+function playRound () {
+    let playerChoice = getPlayerChoice();
+    console.log(`Player: ${playerChoice}`);
+    let computerChoice = getComputerChoice();
+    console.log(`Computer: ${computerChoice}`);
+
+    
+    if (playerChoice === "Rock" && computerChoice === "Scissors" ||  // player wins cases
+        playerChoice === "Scissors" && computerChoice === "Paper" ||
+        playerChoice === "Paper" && computerChoice === "Rock") { 
+        return `You win: ${playerChoice} beats ${computerChoice}`;
+    }
+    else if (computerChoice === "Rock" && playerChoice === "Scissors" ||  // computer wins cases
+    computerChoice === "Scissors" && playerChoice === "Paper" ||
+    computerChoice === "Paper" && playerChoice === "Rock") { 
+    return `You lose: ${computerChoice} beats ${playerChoice}`;
+}
+    else if (computerChoice === playerChoice){
+        return "It's a tie!";
+    } 
+    
+}
+
+console.log(playRound());
+console.log(playRound());
+
+//`Player: ${playerChoice}`; for ease of reading on console later
